ecdb7efec43bca34fbcb59d8a1852643c7529981
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-11 16:37:49 -0500

    search page-template initial commit

---
+++ page-search.php
@@ -0,0 +1,13 @@
+<?php
+get_header();
+?>
+	
+	<div class="container mt-5"> <!-- Start Page Container Div -->
+		<?php
+		get_search_form();
+		?>
+	
+	</div> <!-- End Page Container Div -->
+
+
+<?php get_footer(); ?>
