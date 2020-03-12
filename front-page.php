bcdb7c1efb2a17f2fb2601af7f571797d597ed9b
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-12 11:51:33 -0500

    executed new blog query on front page

--- front-page.php
+++ front-page.php
@@ -19,8 +19,8 @@ get_header();
 	
 	<div class="album py-5 bg-light">
 		<div class="container">
-			<div class="text-center">
-				<h2 style="margin-bottom: 35px;">Upcoming Shows</h2>
+			<div class="text-center mb-4">
+				<h2 class="headline--medium">Upcoming Shows</h2>
 			</div>
 			
 			<div class="row">
@@ -65,7 +65,7 @@ get_header();
 								<p class="card-text"><?php echo wp_trim_words(get_the_content(), 15); ?></p>
 								<div class="d-flex justify-content-between align-items-center">
 									<div class="btn-group">
-										<button type="button" class="btn btn-sm btn-outline-secondary">Tickets</button>
+										<button type="button" class="btn btn-sm btn-outline-secondary"><a href="<?php echo site_url('/Tickets'); ?>">Tickets</a></button>
 										<button type="button" class="btn btn-sm btn-outline-secondary">Music Sample</button>
 									</div>
 								</div>
@@ -77,11 +77,61 @@ get_header();
 				
 			</div>
 			
-			<div class="row">
+			<div class="row mb-5">
 				<div class="col text-center">
 					<a class="anchor-regular" href="<?php echo site_url('/shows'); ?>">See All Shows</a>
 				</div>
 			</div>
+		</div>
+		
+		<div class="container">
+			<div class="text-center mb-4">
+				<h2 class="headline--medium">From Our Blog</h2>
+			</div>
+			<div class="row">
+				<div class="col">
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<?php
+							$firstSlide = true;
+							$blogQuery = new WP_Query(array(
+								'post_type' => 'post',
+								'posts_per_page' => 6
+							));
+							
+							while($blogQuery->have_posts()) {
+								$blogQuery->the_post();	?>
+							<div class="carousel-item <?php if ($firstSlide == true) {
+								echo 'active';
+								$firstSlide = false;
+							} else {
+								$firstSlide = false;
+							} ?>">
+								<img class="d-block w-100" src="<?php echo get_template_directory_uri() . '/img/main-stage.jpg'; ?>" alt="slide">
+								<div class="carousel-caption d-none d-md-block">
+									<h5><?php the_title(); ?></h5>
+									<p><?php echo wp_trim_words(get_the_content(), 15); ?></p>
+								</div>
+							</div>
+							<?php } ?>
+						</div>
+						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			
 		</div>
 	</div>
 
