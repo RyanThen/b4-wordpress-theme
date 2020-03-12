fc8500d9249df060ed103578628ccee4e07c5615
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-12 12:41:50 -0500

    Added custom image sizes

--- functions.php
+++ functions.php
@@ -23,6 +23,10 @@ add_action('wp_enqueue_scripts', 'dnd_files');
 function dnd_theme_features() {
 	add_theme_support('title-tag');
 	add_theme_support('post-thumbnails');
+	
+	add_image_size('imageLandscapeSmall', 350, 250, true);
+	add_image_size('imagePortraitSmall', 480, 650, true);
+	add_image_size('pageBanner', 1500, 350, true);
 }
 add_action('after_setup_theme', 'dnd_theme_features');
 
