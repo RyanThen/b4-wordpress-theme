01d76736f1b34c335a9c4bfcd0f384e8fcdb87b3
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-12 22:51:56 -0500

    added hr to top of footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<hr class="mt-5">
+<footer class="text-muted pt-4 pb-5">
+	<div class="container">
+		<p class="float-right">
+			<a href="#">Back to top</a>
+		</p>
+		<p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+		<p>New to Bootstrap? <a href="https://getbootstrap.com/">Visit the homepage</a> or read our <a href="/docs/4.4/getting-started/introduction/">getting started guide</a>.</p>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
