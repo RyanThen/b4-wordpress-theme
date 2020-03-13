fa7f2ab67faf887ffc8bfef35e691074ec3a8160
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-13 16:34:28 -0500

    minor modifications to template

--- page-tickets.php
+++ page-tickets.php
@@ -6,7 +6,12 @@ get_header();
 		
 		<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
 			<h1 class="display-4"><?php the_title(); ?></h1>
-			<?php while(have_posts()) {
+			<?php
+			$ticket_price_1 = get_field('ticket_price_1');
+			$ticket_price_2 = get_field('ticket_price_2');
+			$ticket_price_3 = get_field('ticket_price_3');
+			
+			while(have_posts()) {
 				the_post();?>
 				<p class="lead"><?php the_content(); ?></p>
 		</div>
@@ -15,10 +20,40 @@ get_header();
 			<div class="card-deck mb-3 text-center">
 				<div class="card mb-4 box-shadow">
 					<div class="card-header">
-						<h4 class="my-0 font-weight-normal">Pro</h4>
+						<h4 class="my-0 font-weight-normal">General</h4>
 					</div>
 					<div class="card-body">
-						<h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+						<h1 class="card-title pricing-card-title">$<?php echo $ticket_price_1; ?></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>20 users included</li>
+							<li>10 GB of storage</li>
+							<li>Priority email support</li>
+							<li>Help center access</li>
+						</ul>
+						<button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+					</div>
+				</div>
+				<div class="card mb-4 box-shadow">
+					<div class="card-header">
+						<h4 class="my-0 font-weight-normal">Pavilion</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$<?php echo $ticket_price_2; ?></h1>
+						<ul class="list-unstyled mt-3 mb-4">
+							<li>20 users included</li>
+							<li>10 GB of storage</li>
+							<li>Priority email support</li>
+							<li>Help center access</li>
+						</ul>
+						<button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+					</div>
+				</div>
+				<div class="card mb-4 box-shadow">
+					<div class="card-header">
+						<h4 class="my-0 font-weight-normal">VIP</h4>
+					</div>
+					<div class="card-body">
+						<h1 class="card-title pricing-card-title">$<?php echo $ticket_price_3; ?></h1>
 						<ul class="list-unstyled mt-3 mb-4">
 							<li>20 users included</li>
 							<li>10 GB of storage</li>
