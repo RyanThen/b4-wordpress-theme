9053a6a97a71e795fc2a67f6dc4692d1a7aa181d
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-14 12:54:43 -0500

    Added featured images

--- archive.php
+++ archive.php
@@ -1,8 +1,19 @@
 <?php get_header(); ?>
 
+<?php while(have_posts()){
+	the_post(); ?>
 <div class="container mt-5">
 	<h1><?php the_archive_title(); ?></h1>
+	<?php if ( has_post_thumbnail() ) { ?>
+		<div class="card-img-top show-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div>
+	<?php } else { ?>
+		<svg class="bd-placeholder-img card-img-top" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em"><?php the_title(); ?></text></svg>
+	<?php } ?>
 	<p class="pt-5 pb-5"><?php echo get_the_archive_description(); ?></p>
 </div>
 
-<?php get_footer(); ?>
+<?php }
+
+get_footer(); ?>
--- single.php
+++ single.php
@@ -6,7 +6,13 @@
 		the_post(); ?>
 		
 		<h2 class="mt-5"><?php the_title(); ?></h2>
-		
+		<?php if ( has_post_thumbnail() ) { ?>
+			<div class="card-img-top show-thumbnail mt-4">
+				<?php the_post_thumbnail(); ?>
+			</div>
+		<?php } else { ?>
+			<svg class="bd-placeholder-img card-img-top mt-4" width="100%" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em"><?php the_title(); ?></text></svg>
+		<?php } ?>
 		<div class="mt-5 pb-5">
 			<?php the_content(); ?>
 		</div>
