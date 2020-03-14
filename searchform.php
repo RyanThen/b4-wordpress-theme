91faf267111cc93c8e5306e0bc922e31c2b187b5
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-14 18:43:30 -0500

    new searchform (bootstrap component)

--- searchform.php
+++ searchform.php
@@ -1,7 +1,9 @@
-<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-	<label class="headline headline--medium" for="s">Perform your search</label>
-	<div class="search-form-row">
-		<input class="s" type="search" name="s" id="s" placeholder="What are you looking for?">
-		<input class="search-submit" type="submit" value="Submit">
+<form class="search-form text-center pt-5 pb-5" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+	<label class="headline-medium" for="s">Perform your search</label>
+	<div class="input-group mb-3 search-input-group">
+		<input type="text" class="s form-control the-search" type="search" name="s" id="s" placeholder="What are you looking for?">
+		<div class="input-group-append">
+			<input class="btn btn-outline-secondary" type="submit" value="Submit">
+		</div>
 	</div>
 </form>
