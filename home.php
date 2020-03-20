e674a5983383c2747d236722fece8a935c930af0
Ryan <39573308+RyanThen@users.noreply.github.com>
2020-03-20 11:35:26 -0500

    incorporated featured image for featured blog on blog home template

--- home.php
+++ home.php
@@ -19,7 +19,8 @@
 		<?php while ( $featured_blog_post->have_posts() ) : $featured_blog_post->the_post(); ?>
 			
 			<?php if ( get_field( 'featured_post' ) ): ?>
-				<div class="jumbotron mt-5 mb-4 p-4 p-md-5 text-white rounded bg-dark">
+				<div class="jumbotron mt-5 mb-4 p-4 p-md-5 text-white rounded bg-dark home-jumbo" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>');">
+					<div class="overlay"></div>
 					<div class="col-md-6 px-0">
 						<h1 class="display-4 font-italic"><?php the_title() ?></h1>
 						<p class="lead my-3"><?php echo wp_trim_words(get_the_content(), 30); ?></p>
