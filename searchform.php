d5ad4071243770e1fecf1f19faecdd44656e018c
Ryan <39573308+RyanThen@users.noreply.github.com>
2021-05-21 15:01:27 -0500

    modified input and added clear button

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form class="search-form text-center pt-5 pb-5" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+<!-- <form class="search-form text-center pt-5 pb-5" method="get" action="<?php echo esc_url(site_url('/')); ?>">
 	<label class="headline-medium" for="s">Perform your search</label>
 	<div class="input-group mb-3 search-input-group">
 		<input type="text" class="s form-control the-search" type="search" name="s" id="s" placeholder="What are you looking for?">
@@ -6,4 +6,17 @@
 			<input class="btn btn-outline-secondary" type="submit" value="Submit">
 		</div>
 	</div>
+</form> -->
+
+<form class="search-form text-center pt-5 pb-5" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+	<label class="headline-medium" for="s">Perform your search</label>
+	
+	<div class="input-group mb-3 search-input-group">
+	<input type="text" class="s form-control the-search" type="search" name="s" id="s" placeholder="What are you looking for?">
+		<div class="input-group-append">
+			<input class="btn btn-outline-secondary submit-search" type="submit" value="Submit">
+			<button class="btn btn-outline-secondary clear-search" type="button">Clear Form</button>
+		</div>
+	</div>
+
 </form>
